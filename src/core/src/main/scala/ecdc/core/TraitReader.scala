cca599bdfd3183adc0b27c40d402d9922f55073f
Ben Simpson <benfrasersimpson@gmail.com>
2015-09-24 00:03:18 +0200

    Initial trait code to make tests pass

--- src/core/src/main/scala/ecdc/core/TraitReader.scala
+++ src/core/src/main/scala/ecdc/core/TraitReader.scala
@@ -1,10 +1,14 @@
 package ecdc.core
 
 import java.io.File
+import scala.io.Source
 
 object TraitReader {
 
   case class ServiceTrait(name: String)
 
-  def readTraits(f: File): Seq[ServiceTrait] = ???
+  def readTraits(f: File): Seq[ServiceTrait] = {
+    for (line <- Source.fromFile(f).getLines().toSeq)
+      yield ServiceTrait(line)
+  }
 }
--- src/core/src/main/scala/ecdc/core/VariableResolver.scala
+++ src/core/src/main/scala/ecdc/core/VariableResolver.scala
@@ -1,11 +1,25 @@
 package ecdc.core
 
 import ecdc.core.TraitReader.ServiceTrait
+import java.io.File
 import model.Cluster
+import scala.io.Source
 
 object VariableResolver {
 
   case class Variable(name: String, value: String, path: String)
 
-  def resolveVariables(traits: Seq[ServiceTrait], cluster: Cluster): Set[Variable] = ???
+  def resolveVariables(baseDir: File, traits: Seq[ServiceTrait], cluster: Cluster): Set[Variable] = {
+    traits.map {
+      t =>
+        val path = s"trait/${t.name}/cluster/${cluster.name}/var"
+        val dir = new File(baseDir, path)
+        Option(dir.list()).map(_.toSeq).getOrElse(Seq()).map(file => toVariable(new File(dir, file), path))
+    }.toSet.flatten
+  }
+
+  private def toVariable(file: File, path: String): Variable = {
+    val value = Source.fromFile(file).getLines.find(_ => true)
+    Variable(file.getName, value.getOrElse(""), path)
+  }
 }
