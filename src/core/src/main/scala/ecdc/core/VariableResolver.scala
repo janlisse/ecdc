f3c6fc11509a326abcc707ba6c9b30d8849cfacb
Ben Simpson <benfrasersimpson@gmail.com>
2015-09-27 22:35:17 +0200

    Change test back to expected value and fix Set duplication

--- src/core/src/main/scala/ecdc/core/VariableResolver.scala
+++ src/core/src/main/scala/ecdc/core/VariableResolver.scala
@@ -1,13 +1,25 @@
 package ecdc.core
 
+import config.Arm
 import ecdc.core.TraitReader.ServiceTrait
 import java.io.File
 import model.Cluster
 import scala.io.Source
 
-object VariableResolver {
+object VariableResolver extends Arm {
 
-  case class Variable(name: String, value: String, path: String)
+  case class Variable(name: String, value: String, path: String) {
+    override def equals(that: scala.Any): Boolean = {
+      that match {
+        case that: Variable => that.canEqual(this) && that.name == name
+        case _ => false
+      }
+    }
+
+    override def hashCode(): Int = 13 * name.hashCode
+
+    override def canEqual(that: Any): Boolean = that.isInstanceOf[Variable]
+  }
 
   def resolveVariables(baseDir: File, traits: Seq[ServiceTrait], cluster: Cluster): Set[Variable] = {
     traits.map {
@@ -15,11 +27,11 @@ object VariableResolver {
         val path = s"trait/${t.name}/cluster/${cluster.name}/var"
         val dir = new File(baseDir, path)
         Option(dir.list()).map(_.toSeq).getOrElse(Seq()).map(file => toVariable(new File(dir, file), path))
-    }.toSet.flatten
+    }.flatten.toSet
   }
 
   private def toVariable(file: File, path: String): Variable = {
-    val value = Source.fromFile(file).getLines.find(_ => true)
+    val value = using(Source.fromFile(file))(_.getLines.find(_ => true))
     Variable(file.getName, value.getOrElse(""), path)
   }
 }
--- src/core/src/test/scala/ecdc/core/VariableResolverSpec.scala
+++ src/core/src/test/scala/ecdc/core/VariableResolverSpec.scala
@@ -9,7 +9,7 @@ import testutils.Spec
 class VariableResolverSpec extends Spec {
   it should "resolve variables for traits" in {
     val vars = resolveVariables(new File("./src/core/src/test/resources/testRepo"), Seq(ServiceTrait("logging"), ServiceTrait("syslog")), Cluster("production"))
-    vars should have size 3
+    vars should have size 2
     vars should contain(Variable("LOGGING_SERVICE_KEY", "logging_service_key_production", "trait/logging/cluster/production/var"))
   }
 }
