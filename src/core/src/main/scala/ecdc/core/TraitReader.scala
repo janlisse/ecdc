e6a498a54190da02a946669ec294a0c998099ca0
janlisse <janlisse@example.com>
2015-11-19 22:23:03 +0100

    log warning if trait directory doesn't exist

--- src/core/src/main/scala/ecdc/core/TraitReader.scala
+++ src/core/src/main/scala/ecdc/core/TraitReader.scala
@@ -5,12 +5,15 @@ import java.util
 
 import com.typesafe.config.ConfigFactory
 import model.Service
+import org.slf4j.LoggerFactory
 import scala.collection.JavaConverters._
 import scala.util.Try
+import TraitReader._
 
 case class ServiceTrait(name: String)
 
 case class TraitReader(service: Service, repoDir: File) {
+
 
   val traitConf = repoDir.toPath.resolve(s"service/${service.name}/trait.conf").toFile
 
@@ -19,6 +22,20 @@ case class TraitReader(service: Service, repoDir: File) {
       .parseFile(traitConf)
       .getStringList("traits")).getOrElse(new util.ArrayList())
       .asScala
+      .filter(traitDirectoryExists(_, repoDir))
       .map(ServiceTrait)
   }
+}
+
+object TraitReader {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
+  def traitDirectoryExists(t: String, repoDir: File): Boolean =
+    if (new File(repoDir, s"trait/$t").exists()) true
+    else {
+      logger.warn(s"Unable to read trait directory: trait/$t")
+      false
+    }
+
 }
