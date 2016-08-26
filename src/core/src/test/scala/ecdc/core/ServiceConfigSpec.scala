0598a652f785c4eebe88f4f53e5759ab69b89365
anho <me@anho.github.com>
2016-08-27 00:42:21 +0200

    add ability to use multiple containers per taskdef

--- src/core/src/test/scala/ecdc/core/ServiceConfigSpec.scala
+++ src/core/src/test/scala/ecdc/core/ServiceConfigSpec.scala
@@ -3,7 +3,8 @@ package ecdc.core
 import java.io.File
 
 import com.typesafe.config.ConfigFactory
-import ecdc.core.TaskDef.{ Environment, LogConfiguration }
+import ecdc.core.TaskDef.ContainerDefinition.Image
+import ecdc.core.TaskDef.{ Environment, LogConfiguration, PortMapping }
 import model.{ Cluster, Service, Version }
 import testutils.Spec
 
@@ -22,14 +23,14 @@ class ServiceConfigSpec extends Spec {
   }
 
   it should "replace taskdef placeholders with variables" in {
-    val sc = ServiceConfig.read(service, cluster, version, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(service, cluster, version, baseDir, defaultVars)
     val td = sc.taskDefinition
     td.containerDefinitions.head.memory shouldBe 1024
     td.containerDefinitions.head.command shouldBe Seq("-Dlogger.resource=production/logback.xml")
   }
 
   it should "add all variables as environment vars" in {
-    val sc = ServiceConfig.read(service, cluster, version, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(service, cluster, version, baseDir, defaultVars)
     val td = sc.taskDefinition
     td.containerDefinitions.head.environment shouldBe Seq(
       Environment("MEMORY", "1024"),
@@ -45,7 +46,7 @@ class ServiceConfigSpec extends Spec {
   }
 
   it should "read loadBalancer config" in {
-    val sc = ServiceConfig.read(Service("bar"), cluster, version, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(Service("bar"), cluster, version, baseDir, defaultVars)
     val lb = sc.loadBalancer
     lb shouldBe Some(LoadBalancer(
       instancePort = 9007,
@@ -68,14 +69,14 @@ class ServiceConfigSpec extends Spec {
 
   it should "use the specified version" in {
     val specificVersion = Version("45.crazysha1")
-    val sc = ServiceConfig.read(Service("bar"), cluster, specificVersion, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(Service("bar"), cluster, specificVersion, baseDir, defaultVars)
     val td = sc.taskDefinition
 
     td.containerDefinitions.head.image.tag shouldBe "45.crazysha1"
   }
 
   it should "extract log configuration" in {
-    val sc = ServiceConfig.read(Service("bar"), cluster, Version.latest, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(Service("bar"), cluster, Version.latest, baseDir, defaultVars)
     val cd = sc.taskDefinition.containerDefinitions.head
 
     cd.logConfiguration shouldBe Some(LogConfiguration(
@@ -87,16 +88,44 @@ class ServiceConfigSpec extends Spec {
   behavior of "taskRoleArn handling"
 
   it should "do not use role arn if missing" in {
-    val sc = ServiceConfig.read(Service("bar"), cluster, Version.latest, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(Service("bar"), cluster, Version.latest, baseDir, defaultVars)
     val td = sc.taskDefinition
 
     td.taskRoleArn shouldBe None
   }
 
   it should "use a role arn if provided" in {
-    val sc = ServiceConfig.read(Service("with-role"), cluster, Version.latest, baseDir, defaultVars, Nil)
+    val sc = ServiceConfig.read(Service("with-role"), cluster, Version.latest, baseDir, defaultVars)
     val td = sc.taskDefinition
 
     td.taskRoleArn shouldEqual Some("arn:aws:iam::xxxxxxxxxxxx:role/test-task-role-arn")
+  }
+
+  it should "extract multiple container definitions" in {
+    val sc = ServiceConfig.read(Service("multiple-containers"), cluster, Version.latest, baseDir, defaultVars)
+    val td = sc.taskDefinition
+
+    td.containerDefinitions should have size 2
+    val first +: second +: Nil = td.containerDefinitions
+
+    first should have(
+      'name("first"),
+      'image(Image(Some("ecdc"), "node", Version.latest.value)),
+      'memory(1024),
+      'essential(true),
+      'entryPoint(Seq("node")),
+      'command(Seq("script.js")),
+      'portMappings(Seq(PortMapping(containerPort = 9000)))
+    )
+
+    second should have(
+      'name("second"),
+      'image(Image(Some("ecdc"), "oracle-jre", "8")),
+      'memory(1024),
+      'essential(true),
+      'entryPoint(Seq("java")),
+      'command(Seq("-Dfoo.bar=yeah")),
+      'portMappings(Seq(PortMapping(containerPort = 9001)))
+    )
   }
 }
